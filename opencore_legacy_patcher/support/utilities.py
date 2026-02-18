26db810170f384e754b564c7f04ca47682f39708
YBronst <157227982+YBronst@users.noreply.github.com>
2026-02-18 10:58:58 +0200

    Modify comment in check_filesystem_type function
    Updated comment to reflect that only 'apfs' is expected.
    
    Signed-off-by: YBronst <157227982+YBronst@users.noreply.github.com>

--- opencore_legacy_patcher/support/utilities.py
+++ opencore_legacy_patcher/support/utilities.py
@@ -134,7 +134,7 @@ def check_seal():
         return False
 
 def check_filesystem_type():
-    # Expected to return 'apfs' or 'hfs'
+    # Expected to return 'apfs'
     filesystem_type = plistlib.loads(subprocess.run(["/usr/sbin/diskutil", "info", "-plist", "/"], stdout=subprocess.PIPE).stdout.decode().strip().encode())
     return filesystem_type["FilesystemType"]
 
